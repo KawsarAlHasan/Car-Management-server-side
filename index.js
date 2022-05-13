ddadefe95a7a96f289cddce9f9d25d81a97419d1
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-05-14 00:57:25 +0600

    load all inventories and create single inventorie

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -12,14 +12,35 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://dbUser1:<password>@cluster0.dfuca.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dfuca.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('warehouse DB connected');
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const inventoriesCollection = client.db('Inventories1').collection('InventoriesItem1');
+        
+        app.get('/inventorie', async (req, res) => {
+            const query = {};
+            const cursor = inventoriesCollection.find(query);
+            const inventories = await cursor.toArray();
+            res.send(inventories);
+        });
+
+        app.get('/inventorie/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const inventory =  await inventoriesCollection.findOne(query);
+            res.send(inventory);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 
 
