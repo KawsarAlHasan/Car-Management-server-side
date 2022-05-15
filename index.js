6af41fc24cac1351fdf41097e0fd03419465cda1
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-05-15 17:33:53 +0600

    added delete inventorie

--- index.js
+++ index.js
@@ -41,6 +41,14 @@ async function run() {
             res.send(result);
         });
 
+        //Delete
+        app.delete('/inventorie/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await inventoriesCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally {
 
